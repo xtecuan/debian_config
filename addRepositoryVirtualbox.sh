5e1cbbe9642084f052e7b8a30660cddc9938b3fc
Tadeo Rivera <xtecuan@gmail.com>
2015-11-23 00:33:36 -0500

    More packages and Virtualbox

---
+++ addRepositoryVirtualbox.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian jessie contrib" >> /etc/apt/sources.list'
+sudo apt-get update
---
+++ addVirtualKeyOracle.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
